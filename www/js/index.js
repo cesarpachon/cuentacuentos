2abc0b4becd8d8847c9d53c811aad59de9768b79
cesarpachon <cesarpachon@gmail.com>
2014-12-01 23:00:52 -0500

    sorting files to proper firing of jquery mobile init event

--- www/js/index.js
+++ www/js/index.js
@@ -55,11 +55,21 @@ app.initialize();
 app.onDeviceReady();
 
 
-$(document).bind("mobileinit", function($) {
+$(document).bind("mobileinit", function(ev) {
   console.log("mobile init!");
   //here.. general configuration for jquery mobile, like apply themes
-  $.mobile.defaultPageTransition= "slide";
+  //$.mobile.defaultPageTransition= "slide";
   //$.mobile.toolbar.prototype.options.addBackBtn = true;
   //$.mobile.toolbar.prototype.options.backBtnText = "Back";
 
+
+  $( "#page_books" ).pagecontainer({
+load: function( event, ui ) {
+  console.log(" books loaded!" );
+}
 });
+
+
+});
+
+
